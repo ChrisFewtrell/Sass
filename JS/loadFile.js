d84a5ec3b174ee5f164bc7f4df3f0fa0ff97f4e6
chrisfewtrell <chris.fewtrell@grantadesign.com>
2013-03-12 21:31:56 +0000

    Loading files directly

---
+++ JS/loadFile.js
@@ -0,0 +1,32 @@
+﻿(function ($) {
+    var loadfileAttribute = "loadFile";
+
+    var loadFile = function () {
+        var $el = $(this);
+        var filename = $el.attr(loadfileAttribute);
+        console.log("Load file:" + filename);
+        var promise = $.get(filename, null, function (data) {
+            if (typeof prettyPrint === "undefined") {
+                console.log("Prettyprint not loaded");
+                $el.text(data);
+                return;
+            }
+            $el.text(data);
+            //console.log("prettyPrint");
+            //var html = prettyPrintOne(data);
+            //$el.html(html);
+        });
+        return promise;
+    };
+
+    var loadAllFiles = function () {
+        var elementsToLoad = $("[" + loadfileAttribute + "]");
+
+        console.log("load all files:" + elementsToLoad.length);
+        $.each(elementsToLoad, function () {
+            loadFile.apply(this);
+        });
+    };
+    
+    $(loadAllFiles);
+})(jQuery);
